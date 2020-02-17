d2b14d609de2f7e11f6f5c87de0b58b2ef3c8b90
Liam Schenck <lschenc41@ccs.local>
2020-02-17 12:30:19 -0500

    Work on PID code

--- Code/PID_GameBoy_RoughDraft.ino
+++ Code/PID_GameBoy_RoughDraft.ino
@@ -1,7 +1,7 @@
 LiquidCrystal_I2C lcd(0x3F,2,1,0,4,5,6,7);
 
 #define LED_SWITCH_PIN 12
-bool LED_SWITCH_PIN_STATE;
+bool LED_SWITCH_STATE;
 #define LED_BACKLIGHT_PIN 9
 #define BUTTON_INCREASE_PIN 8
 bool BUTTON_INCREASE_STATE;
@@ -9,6 +9,11 @@ bool BUTTON_INCREASE_STATE;
 bool BUTTON_DECREASE_STATE;
 int brightness;
 #define LCD_BACKLIGHT_PIN 3
+
+// temporarily unorganized vars
+#define PID_ENABLE_SWITCH_PIN 6; // change this pin to whatever
+bool PID_ENABLE_SWITCH_STATE;
+bool PID_enabled;
 
 void setup() {
 	Serial.begin(9600);
@@ -37,8 +42,8 @@ void loop() {
 	}
 
 	// LED Switch
-	LED_SWITCH_PIN_STATE = digitalRead(LED_SWITCH_PIN);
-	if (LED_SWITCH_PIN_STATE == true) {
+	LED_SWITCH_STATE = digitalRead(LED_SWITCH_PIN);
+	if (LED_SWITCH_STATE == true) {
 		analogWrite(LED_BACKLIGHT_PIN, brightness);
 	} else {
 		analogWrite(LED_BACKLIGHT_PIN, 0);
@@ -47,4 +52,11 @@ void loop() {
 	// LCD
 	lcd.setBacklight(brightness);
 
+	// PID
+	PID_ENABLE_SWITCH_STATE = digitalRead(PID_ENABLE_SWITCH_PIN);
+	if (PID_ENABLE_SWITCH_STATE == true) {
+		PID_enabled = true;
+	} else {
+		PID_enabled = false;
+	}
 }
